59f9d379e982b56320b093ea78ca473c74e37623
Diana Avila <45690382+dianafismed@users.noreply.github.com>
2023-09-29 17:51:01 -0300

    up

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,10 +1,11 @@
-function InserirImagem(url) {
+function inserirImagem(url) {
   let img = document.createElement("img");
   url = ('../images/studio/glasses/*.png\.jpg')
   img.src=url;
   document.body.appendChild(img);
 }
 
-function Teste(){
-  document.write(Date());
-}
+//let page = document.getElementsByName('glasses.html');
+
+
+
